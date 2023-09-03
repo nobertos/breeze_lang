5c2dc6acfa9e9a697c664914063aeabb61874f07
nobertos <rn.zorgani@gmail.com>
2023-09-03 22:53:25 +0100

    Edit: fixed some code, implemented VM stack, next is arithmetic/logic operations

--- src/main.c
+++ src/main.c
@@ -3,21 +3,17 @@
 #include "common.h"
 #include "value.h"
 #include "virtual_machine.h"
+#include <stdio.h>
 
 int32_t main() {
   init_vm();
-
   Chunk chunk;
   init_chunk(&chunk);
-  write_value_vec(&chunk.constants, 12);
-  write_chunk(&chunk, OpConstantLong, 124);
-  write_chunk(&chunk, (uint8_t) 0 & 0xff, 124);
-  write_chunk(&chunk, (uint8_t) 0 & 0xff, 124);
-  write_chunk(&chunk, (uint8_t) 0 & 0xff, 124);
+  push_constant(&chunk, 1.2, 123);
+  write_chunk(&chunk, OpNegate, 123);
 
   write_chunk(&chunk, OpReturn, 124);
-
-  disassemble_chunk(&chunk, "test chunk");
+  // disassemble_chunk(&chunk, "chunk");
   interpret(&chunk);
   free_vm();
   free_chunk(&chunk);
--- src/virtual_machine.h
+++ src/virtual_machine.h
@@ -2,10 +2,15 @@
 #define breeze_virtual_machine_h
 
 #include "chunk.h"
+#include "value.h"
+
+#define STACK_MAX 256
 
 typedef struct {
   Chunk* chunk;
   uint8_t* inst_ptr;
+  Value stack[STACK_MAX];
+  Value* stack_ptr;
 } VirtualMachine;
 
 typedef enum {
@@ -17,5 +22,7 @@ typedef enum {
 void init_vm();
 void free_vm();
 InterpretResult interpret(Chunk* chunk);
+void push_stack(Value value);
+Value pop_stack();
 
 #endif // !breeze_virtual_machine_h
