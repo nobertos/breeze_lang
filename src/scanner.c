7eba5b8594da1560a08ceddc14194a8db2e21807
nobertos <rn.zorgani@gmail.com>
2023-09-07 00:28:58 +0100

    Edit: implemented keyword scanning

--- src/scanner.c
+++ src/scanner.c
@@ -18,7 +18,6 @@ void init_scanner(const char *source) {
   scanner.line = 1;
 }
 
-static bool is_at_end() { return *scanner.current == '\0'; }
 
 static Token make_token(TokenType type) {
   Token token;
@@ -43,46 +42,220 @@ static const char advance() {
   return *(scanner.current - 1);
 }
 
+static const char peek() {
+  return *(scanner.current);
+}
+
+static bool is_at_end() { return peek() == '\0'; }
+
+static const char peek_next() {
+  if (is_at_end()) {
+    return '\0';
+  }
+  return *(scanner.current + 1);
+}
+
+
+static bool match(const char expected) {
+  if (is_at_end()) {
+    return false;
+  }
+  if (peek()!= expected) {
+    return false;
+  }
+  advance();
+  return false;
+}
+
+static void skip_white_space() {
+  while(true) {
+    const char c = peek();
+    switch (c) {
+      case ' ':
+      case '\r':
+      case '\t': {
+        advance();
+        break;
+      } 
+      case '\n': {
+        scanner.line += 1;
+        advance();
+        break;
+      }
+      case '/': {
+        if (peek_next() == '/') {
+          while (peek() != '\n' && !is_at_end()) {
+            advance(); 
+          }
+        } else {
+          return;
+        }
+        break;
+      }
+      default:
+        return;
+    }    
+  }
+}
+
+static Token string() {
+  while(peek() != '"' && !is_at_end()) {
+    if (peek() == '\n') {
+      scanner.line += 1;
+    }
+    advance();
+  }
+
+  if (is_at_end()) {
+    return error_token("Unterminated string.");
+  }
+  
+  advance();
+  return make_token(TokenString);
+}
+
+static bool is_digit(const char c) {
+  return c >= '0' && c <= '9';
+}
+
+static Token number() {
+  while (is_digit(peek())){
+    advance();
+  }
+
+  if (peek() == '.' && is_digit(peek_next())) {
+    advance();
+    while (is_digit(peek())) {
+      advance(); 
+    }
+  }
+
+  return make_token(TokenNumber);
+}
+
+static bool is_alpha(const char c) {
+  return ((c >= 'a' && c <= 'z') ||
+          (c >= 'A' && c <= 'Z') ||
+          (c == '_'));
+}
+
+static TokenType check_keyword(uint8_t start, uint8_t len,
+                               const char * rest, TokenType type){
+
+  if ((scanner.current - scanner.start == start + len) &&
+      memcmp(scanner.start + start, rest, len) == 0){
+    return type;
+  }
+
+  return TokenIdentifier;
+}
+static TokenType identifier_type () {
+  switch (scanner.start[0]) {
+    case 'a': return check_keyword(1, 2, "nd", TokenAnd);
+    case 'c': return check_keyword(1, 4, "lass", TokenClass);
+    case 'e': return check_keyword(1, 3, "lse", TokenElse);
+    case 'f': {
+      if (scanner.current - scanner.start > 1) {
+        switch (scanner.start[1]) {
+          case 'a': return check_keyword(2, 3, "lse", TokenFalse);
+          case 'o': return check_keyword(2, 1, "r", TokenFor);
+          case 'n': return check_keyword(2, 0, "", TokenFn);
+        }
+      }
+      break;
+    }
+    case 'i': return check_keyword(1, 1, "f", TokenIf);
+    case 'l': return check_keyword(1, 2, "et", TokenLet);
+    case 'n': return check_keyword(1, 3, "ull", TokenNull);
+    case 'o': return check_keyword(1, 1, "r", TokenOr);
+    case 'p': return check_keyword(1, 4, "rint", TokenPrint);
+    case 'r': return check_keyword(1, 5, "eturn", TokenReturn);
+    case 's': return check_keyword(1, 4, "uper", TokenSuper);
+    case 't': {
+      if (scanner.current - scanner.start > 1) {
+        switch (scanner.start[1]) {
+          case 'h': return check_keyword(2, 2, "is", TokenThis);
+          case 'r': return check_keyword(2, 2, "ue", TokenTrue);
+        }
+      }
+      break;
+    }
+    case 'w': return check_keyword(1, 4, "hile", TokenWhile);
+  }
+  return TokenIdentifier;
+}
+
+static Token identifier() {
+  while(is_alpha(peek()) || is_digit(peek())) {
+    advance();
+  }
+  return make_token(identifier_type());
+}
+
 Token scan_token() {
+  skip_white_space();
   scanner.start = scanner.current;
   if (is_at_end()) {
     return make_token(TokenEof);
   }
   const char c = advance();
 
+  if (is_alpha(c)){
+    return identifier();
+  }
+
+  if (is_digit(c)) {
+    return number();
+  }
+
   switch (c) {
-    case '(' : {
+    case '(': {
       return make_token(TokenLeftParen);
     }
-    case ')' : {
+    case ')': {
       return make_token(TokenRightParen);
     }
-    case '{' : {
+    case '{': {
       return make_token(TokenLeftBrace);
     }
-    case '}' : {
+    case '}': {
       return make_token(TokenRightBrace);
     }
-    case ';' : {
+    case ';': {
       return make_token(TokenSemiColon);
     }
-    case ',' : {
+    case ',': {
       return make_token(TokenComma);
     }
-    case '.' : {
+    case '.': {
       return make_token(TokenDot);
     }
-    case '-' : {
+    case '-': {
       return make_token(TokenMinus);
     }
-    case '+' : {
+    case '+': {
       return make_token(TokenPlus);
     }
-    case '/' : {
+    case '/': {
       return make_token(TokenSlash);
     }
-    case '*' : {
+    case '*': {
       return make_token(TokenStar);
+    }
+    case '!': {
+      return make_token(match('=') ? TokenBangEqual : TokenBang);
+    }
+    case '=': {
+      return make_token(match('=') ? TokenEqualEqual : TokenEqual);
+    }
+    case '<': {
+      return make_token(match('=') ? TokenLessEqual : TokenLess);
+    }
+    case '>': {
+      return make_token(match('=') ? TokenGreaterEqual : TokenGreater);
+    }
+    case '"': {
+      return string();
     }
   }
   return error_token("Unexpected character.");
